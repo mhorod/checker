9737fc2fb1a3ce7543ce2e97427536890cbea872
mhorod <michalhorodecki2002@gmail.com>
2020-11-12 02:50:08 +0100

    Pylint fixes on top level

--- run-tests.py
+++ run-tests.py
@@ -1,2 +1,3 @@
+# pylint: disable-all
 from simple_checker import run
 run.from_cli()
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
+"""
+Install requirements and setup the application
+"""
 import subprocess
 import sys
 
 
 def install(package):
+    """Install package"""
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
 external_packages = ['termcolor', 'console_progressbar']
-for package in external_packages:
-    install(package)
+for package_name in external_packages:
+    install(package_name)
